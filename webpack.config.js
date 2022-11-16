a580189b65ff74898adef916e907ac44d637b7b9
Can Bartu Kılıç <91592857+cbartuk@users.noreply.github.com>
2022-11-16 14:27:41 +0300

    fix the bugs

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "/js/custom.js",
+  entry: "/dist/main.js",
 
   mode: "development",
 
